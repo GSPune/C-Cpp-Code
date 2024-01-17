b4e8804a0850874c4864fd24326b047f6d39b5a0
Stephen George <stephengeorgepune@gmail.com>
2024-01-17 20:49:23 +0530

    Improved Efficiency

--- ImprovedLimit.cpp
+++ ImprovedLimit.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 
 using namespace std;
-unsigned long long int fact(int);
+long double fact(int);
 /*namespace N
 {
     typedef int n;
@@ -10,40 +10,38 @@ unsigned long long int fact(int);
 int main()
 {
     int n = 1,p = 1;
-    long double pmax = 0, max = 0.1;
+    long double pmax = 0.1, max = 0.1;
     while (max >= pmax)
     {
         n++;
-        if (n % 5 == 0)
-        {
-            p = n/5;
-        }
         pmax = max;
-        max = (long double)fact(n)/pow(10,p);
+        max = fact(n);
     }
-    cout << "The limit reached is " << pmax << " for n equal to " << n << endl;
+    cout << "The limit reached is " << pmax << "+"<<(n/5)<<" for n equal to " << n << endl;
 
     float m = 1.0;
-    long double pmin = 1.0, min = 1.0;
+    long double pmin = 10, min = 10;
     while (min <= pmin)
     {
         m++;
         pmin = min;
         min = (long double)1/fact((int)m);
     }
-    cout << "The limit reached is " << pmin << " for m equal to " << m << endl;
+    cout << "The limit reached is " << pmin << "-"<<(n/5)<< " for m equal to " << m << endl;
 }
 
-unsigned long long int fact(int k)
+long double fact(int k)
 {
-    unsigned long long int f = 1;
+    long double f = 1;
+	int p = 1;
     for (unsigned long long int i = 1; i < k + 1; i++)
     {
-        /*if (i % 5 == 0)
+        if (i % 5 == 0)
         {
-
-        }*/
-        f *= i;
-    }
+           p = i/5;//sort of marker for powers of 10  
+        }
+	 	f *= i; 
+	}
+	f /=pow(10,(p));
    return f;
 }
