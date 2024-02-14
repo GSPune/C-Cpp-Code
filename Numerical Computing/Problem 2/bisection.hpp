f4eb45b5a9374e59d4fb19e665fa3a9c222d1fe7
Stephen George <stephengeorgepune@gmail.com>
2024-02-14 19:12:58 +0530

    Added some formatting + changes

--- Numerical Computing/Problem 2/bisection.hpp
+++ Numerical Computing/Problem 2/bisection.hpp
@@ -1,6 +1,6 @@
 class BisectionMethod
 {
-    double a,b,delta;
+    double a,b,delta;//private
     public:
         BisectionMethod(double);
         void get_interval(double,double);
--- Others/Triangle.cpp
+++ Others/Triangle.cpp
@@ -2,15 +2,13 @@
 #include <cmath>
 #include <cstdlib>
 using namespace std;
-
+#define PI 3.14159265
 class Triangle
 {
     int a,b,c;
     double alpha,beta,gamma;
     public:
-        Triangle(int x,int y,int z):a(x),b(y),c(z){
-            cout << "x,y,z ->" << a << "," << b << "," << c << endl;
-        }
+        Triangle(int x,int y,int z):a(x),b(y),c(z) {}
         bool valid()
         {
             if ((a + b > c) && (a + c > b) && (b + c > a)){
@@ -27,14 +25,19 @@ class Triangle
              alpha = acos((pow(b,2)+pow(c,2)-pow(a,2))/(2*c*b));
 
              cout << "The internal angles of the triangle are ";
-             cout <<  alpha << "," << beta << "," << gamma << endl;
+             cout <<  alpha * 180.0 / PI << ", " << beta * 180.0 / PI << ", " << gamma * 180.0 / PI << endl;
         };
 };
 
 int main(void)
 {
     int x,y,z;
+    cout << "Enter 3 valid sides for the triangle!" << "\n";
     cin >> x >> y >> z;
     Triangle Obj1(x,y,z);
+    if(Obj1.valid())
+        cout << "Triangle is valid." << endl;
+    else
+        cout << "Triangle is not valid." << endl;
     return 0;
 }
