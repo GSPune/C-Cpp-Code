eb06077f9bde4dcbeb92ee683da185eed34ddd4f
Stephen George <stephengeorgepune@gmail.com>
2024-02-15 20:26:53 +0530

    Minor Change

--- Others/main.cpp
+++ Others/main.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main(void)
 {
     ContactManager o1;
-    o1.Add("Stephen","George",9011036227,"sjl@gmail");
-    o1.Add("ACV","TJKA",90312036227,"xyz@gmail");
+    o1.Add("Stephen","George",9062275648,"sjl@gmail");
+    o1.Add("ACV","TJKA",90568036277,"xyz@gmail");
 
     o1.ListAllContacts();
     return 0;
