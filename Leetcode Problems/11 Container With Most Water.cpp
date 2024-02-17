f6e82f7dfb3dcb5b0025911d106dc815027e61f6
Stephen George <stephengeorgepune@gmail.com>
2024-02-17 11:36:35 +0530

    Problem 11(M)

---
+++ Leetcode Problems/11 Container With Most Water.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int p1 = 0,p2 = height.size() - 1;
+        int max_area = 0,new_area = 0;
+        int min_height = min(height[p1],height[p2]);
+        max_area = (p2-p1) * min_height;
+        while(p1 < p2)
+        { 
+            if (min_height == height[p1])
+                p1++;
+            else
+                p2--;
+            min_height = min(height[p1],height[p2]);
+            max_area = max(max_area,(p2-p1) * min_height);
+            //new_area = (p2-p1) * min_height;
+            // if (max_area < new_area)
+            //     max_area = new_area;
+        }
+        return max_area;
+    }
+};
