8122374b2680178390c52183c5705f2d9b364754
Stephen George <stephengeorgepune@gmail.com>
2024-02-19 21:21:57 +0530

    Adapted version for edge case

--- Leetcode Problems/605.cpp
+++ Leetcode Problems/605.cpp
@@ -5,35 +5,35 @@ using namespace std;
 int main(void)
 {
     int n;
-    vector<int> flowerbed = {1,0,0,0,1};
+    vector<int> flowerbed = {1};
     int size = flowerbed.size(), count = 0;
     cout << "Enter the number of flowers to be placed\n";
     cin >> n;
-    
-    for(int i = 0; i < size; i++)
+    if (size == 1)
     {
-        if (i == 0)
+        bool res = (flowerbed[0] == 0) ? count++ >= n : count >= n;
+        cout.setf(ios::boolalpha);
+        cout << res << endl;
+    }
+    else
+    {
+        if (flowerbed[0] == 0 && flowerbed[1] == 0){
+            flowerbed[0] = 1;count++;}
+
+        if (flowerbed[size - 1] == 0 && flowerbed[size - 2] == 0){
+            flowerbed[size - 1] = 1;count++;}
+
+        for(int i = 0; i < size; i++)
         {
-            if (flowerbed[i] == 0 && flowerbed[i + 1] == 0){
-                flowerbed[i] = 1;count++;}
-        }
-        else if (i == size - 1)
-        {
-            if (flowerbed[i] == 0 && flowerbed[i - 1] == 0){
-                flowerbed[i] = 1;count++;}
-        }
-        else
-        {
-            if(flowerbed[i] == 0 && flowerbed[i + 1] == 0 && flowerbed[i - 1] == 0)
-            {
+            if(flowerbed[i] == 0 && flowerbed[i + 1] == 0 && flowerbed[i - 1] == 0){
                 flowerbed[i] = 1;count++;
             }
         }
-    }
-    if (count >= n)
-        cout << "YES!..true\n";
-    else
-        cout << "false\n";
 
+        if (count >= n)
+            cout << "YES!..true\n";
+        else
+            cout << "false\n";
+    }
     return 0;
 }
