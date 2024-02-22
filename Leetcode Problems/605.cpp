00a7f2c3bbdc7471031b037fa4b4f14ccf3ea096
GSPune <84006256+GSPune@users.noreply.github.com>
2024-02-22 08:35:05 +0530

    Updated 605.cpp to handle edge case

--- Leetcode Problems/605.cpp
+++ Leetcode Problems/605.cpp
@@ -23,7 +23,7 @@ int main(void)
         if (flowerbed[size - 1] == 0 && flowerbed[size - 2] == 0){
             flowerbed[size - 1] = 1;count++;}
 
-        for(int i = 0; i < size; i++)
+        for(int i = 1; i < size - 1; i++)
         {
             if(flowerbed[i] == 0 && flowerbed[i + 1] == 0 && flowerbed[i - 1] == 0){
                 flowerbed[i] = 1;count++;
