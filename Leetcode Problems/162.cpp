03f820639bb7828ec10a8d0a417d1e2b310e9dc9
Stephen George <stephengeorgepune@gmail.com>
2024-02-26 09:01:15 +0530

    Improved efficiency

--- Leetcode Problems/162.cpp
+++ Leetcode Problems/162.cpp
@@ -3,34 +3,21 @@ using namespace std;
 
 int main(void)
 {
-    //vector<int> nums = {1,2,1,3,5,6,4};
-    vector<int> nums = {5,3,1,6,4,2};
+    vector<int> nums = {1,2,1,3,5,6,4};
+    //vector<int> nums = {5,3,1,6,4,2};
     int size = nums.size(),left = 0,right,mid;
     right = size - 1;
 
     if (size == 1)
         cout << "0";
 
-    while(left <= right)
-    {
+    while(left <= right){
         mid = (left + right) / 2;
-        if (mid == 0) {
-            if (nums[mid] > nums[mid + 1])
-                cout << mid;
-        }else if (mid == size - 1) {
-            if (nums[mid] > nums[mid - 1])
-                cout << mid;
-        }else {
-            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
-                cout << mid;
-            }
-        }
 
-        if (nums[mid + 1] > nums[mid]) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
+        if ((mid == 0 || nums[mid] > nums[mid - 1]) && (mid == size - 1 || nums[mid] > nums[mid + 1]))
+            cout << mid << endl;
+
+        (nums[mid + 1] > nums[mid]) ? left = mid + 1 : right = mid - 1;
     }
 
 }
