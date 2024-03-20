f6171f6fc8414726ea1a4e63e5c709ead45082da
Stephen George <stephengeorgepune@gmail.com>
2024-03-20 22:45:22 +0530

    Matrix Class

--- Numerical Computing/Problem 3/MatrixClass.hpp
+++ Numerical Computing/Problem 3/MatrixClass.hpp
@@ -1,2 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+class Matrix{
+    public:
+        Matrix();
+        void addition(Matrix,Matrix);
+        void substraction(Matrix,Matrix);
+        void multiply(Matrix,Matrix);
+        void Guassian_elimination(Matrix);
+};
