cf40ffabff0f3a53e0e4c6316cb8c51749b7f97c
Stephen George <stephengeorgepune@gmail.com>
2024-03-25 23:07:07 +0530

    Corrected version -- Sum of Lengths of all Good Words

--- Leetcode Problems/1160.cpp
+++ Leetcode Problems/1160.cpp
@@ -1,29 +1,49 @@
-/*Find (Good) words that can be formed by characters!
+/*Find (Good) words that can be formed by (given)characters!
 Return the sum of lengths of all good strings in words.*/
 #include <bits/stdc++.h>
 using namespace std;
 
 int main(){
-    vector<string> words = {"cat","bt","hat","tree"};
+    vector<string> words = {"catt","bt","hat","tree"};
     string chars = "atach";
     int sum = 0;bool f = false;
 
-    unordered_set<char> s;
+    unordered_map<char,int> m,mcopy;
     for (int i = 0; i < chars.size(); ++i){
-        s.insert(chars[i]);
+        m[chars[i]]++;
+    }
+
+    // mcopy = m;  ?a shallow copy?
+
+    for (int i = 0; i < chars.size(); ++i){
+        mcopy[chars[i]]++;
     }
 
     for (int j = 0; j < words.size(); ++j){
-        //check if each letter exists in set s
+        //check if each letter (of each word in array) exists in map m
         for (const auto &e:words[j]){
-            if (s.find(e) == s.end()){
+            if (m.find(e) == m.end()){
                 f = true;
                 break;
+            }
+            else{
+                unordered_map <char,int>::iterator got = m.find(e);
+                if (got->second > 0)
+                    got->second --;
+                else{
+                    f = true;
+                    break;
+                }
             }      
         } 
         if (!f){
         sum = sum + words[j].length();}
         f = false;
+
+        for (const auto &e:mcopy){
+            m[e.first] = e.second;
+        }
+            
     }
 
     cout << sum << endl;
