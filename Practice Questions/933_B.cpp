38feedcc0d3b1165fd6e097d41347ee6705479f4
Stephen George <stephengeorgepune@gmail.com>
2024-04-19 00:45:31 +0530

    Minor update

---
+++ Practice Questions/933_B.cpp
@@ -0,0 +1,15 @@
+//B. Rudolf and the 121
+
+// Rudolf can apply the given operation any number of times. Any index i
+// can be used zero or more times.
+// Can he make all the elements of the array equal to zero using this operation?
+// a(i−1)=a(i−1)−1
+// ai=ai−2
+// ai+1=ai+1−1
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    return 0;
+}
