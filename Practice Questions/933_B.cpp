d721276d38626f2e41300d701c5d67d1d400c8fb
Stephen George <stephengeorgepune@gmail.com>
2024-04-20 20:29:05 +0530

    Greedy Problem,DP -- Rudolf & 121

--- Practice Questions/933_B.cpp
+++ Practice Questions/933_B.cpp
@@ -5,11 +5,45 @@
 // Can he make all the elements of the array equal to zero using this operation?
 // a(i−1)=a(i−1)−1
 // ai=ai−2
-// ai+1=ai+1−1
+// a(i+1)=a(i+1)−1
 
 #include <bits/stdc++.h>
 using namespace std;
 
+void operation(vector<int> A,int size){
+    int op;
+    for (int i = 0; i < size - 2; i++){
+        op = A[i];
+        if(op > 0){
+            A[i] -= op;
+            A[i+1] -= 2*op;
+            A[i+2] -= op;
+        }
+    }
+
+    for (auto &e:A){
+        if (e != 0){
+            cout << "NO" << endl;
+            return;
+        }
+    }
+    cout << "YES" <<endl;
+}
+
 int main(){
+    int cases,n;
+    //Number of test cases
+    cin >> cases;
+    for (int i = 0; i < cases; i++){
+        //size of test case array
+        cin >> n;
+        vector<int> Input;
+        int ip;
+        for (int j = 0; j < n;j++){
+            cin >> ip;Input.push_back(ip);
+        }
+        operation(Input,n);
+    }
+
     return 0;
 }
