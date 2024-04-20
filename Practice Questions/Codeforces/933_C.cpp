6d86bde0b3a7cb22369cc33aa16ebe5c092d26cc
Stephen George <stephengeorgepune@gmail.com>
2024-04-20 22:34:31 +0530

    Minor addition

---
+++ Practice Questions/Codeforces/933_C.cpp
@@ -0,0 +1,13 @@
+/*Rudolf has a string s of length n. Rudolf considers the string s to be ugly if it contains 
+the substring "pie" or the substring "map", otherwise the string s will be considered beautiful.*/
+
+//For each test case, output a single integer — the minimum number of characters that need to be deleted to make the string s
+//beautiful. If the string is initially beautiful, then output 0.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
