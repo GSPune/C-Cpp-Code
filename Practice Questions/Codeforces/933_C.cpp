3a0f59a1d423b1dd69e94831b9540c2c5e9a608c
Stephen George <stephengeorgepune@gmail.com>
2024-04-23 10:09:01 +0530

    1st Attempt -- C

--- Practice Questions/Codeforces/933_C.cpp
+++ Practice Questions/Codeforces/933_C.cpp
@@ -7,7 +7,28 @@ the substring "pie" or the substring "map", otherwise the string s will be consi
 #include <bits/stdc++.h>
 using namespace std;
 
+void beautify_string(string,int);
+
 int main()
 {
+    int cases,k;
+    //Number of test cases
+    cin >> cases;
+    for (int i = 0; i < cases; i++){
+        //take in the length of the string
+        cin >> k;
+        string s;
+        cin >> s;
+        beautify_string(s,k);
+    }
     return 0;
+}
+
+void beautify_string(string s,int l){
+    int count = 0;
+    for (int i = 0; i < (l - 2); i++){
+        if (s[i] == 'm' && s[i+1] == 'a' && s[i + 2] == 'p' || s[i] == 'p' && s[i+1] == 'i' && s[i + 2] == 'e' )
+            count++;
+    }
+    cout << count << endl;
 }
