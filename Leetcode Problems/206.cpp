4efa4f1019b569aeb6ae890b10b4e107da9b2d99
Stephen George <stephengeorgepune@gmail.com>
2024-04-25 09:57:03 +0530

    1st Attempt -- Rev. LL

---
+++ Leetcode Problems/206.cpp
@@ -0,0 +1,34 @@
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+      int val;
+      ListNode *next;
+      //Member initialization in constructors
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+int main(){
+    ListNode *head;
+    head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+
+    ListNode *temp1 = NULL, *temp2 = head->next;
+
+    while(head!= nullptr){
+        head->next = temp1;
+        temp1 = head;
+        head = temp2;
+        temp2 = head->next;
+    }
+    head -> next = temp1;
+
+    return 0;
+
+}
