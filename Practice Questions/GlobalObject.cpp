569f05ca5497c3792c3001b20f0432bf054e162d
Stephen George <stephengeorgepune@gmail.com>
2024-05-03 21:08:25 +0530

    OOPs Concept Testing

---
+++ Practice Questions/GlobalObject.cpp
@@ -0,0 +1,35 @@
+//Testing some OOPs concepts...
+#include <bits/stdc++.h>
+using namespace std;
+
+class MSC{
+    public:
+        MSC()
+        {
+            cout << "MSc Sci. Comp" << endl;
+        }
+
+        MSC(string s){
+            cout << s << endl;
+        }
+
+        ~MSC(){
+            cout << "Goodbye" << endl;
+        }
+
+};
+
+// MSC a;
+
+//actual start of program is allocation for global objects
+//main is the starting point and the ending point of the application
+//actual end is destructor call at the end of the block
+int main(){
+    //Global object
+    MSC a;
+    {MSC b("Test");}
+    //object is destroyed when the block is deleated
+    cout << "Hello World" << endl;
+    return 0;
+}
+
