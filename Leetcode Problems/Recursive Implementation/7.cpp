0be601ba063a907f1b5eb49f2d426a1e31e24b3a
Stephen George <stephengeorgepune@gmail.com>
2024-05-13 13:15:58 +0530

    Added code for reversing an integer (recursive)

--- Leetcode Problems/Recursive Implementation/7.cpp
+++ Leetcode Problems/Recursive Implementation/7.cpp
@@ -2,14 +2,20 @@
 using namespace std;
 
 int reverse(int x){
+    const int MAX = std::numeric_limits<int>::max();
+    const int MIN = std::numeric_limits<int>::min();
+    // int prev = x;
+    if (x == 0)
+        return 0;
     int len = to_string(abs(x)).length();
-    int ans;
+    if ((reverse(x/10) + (x % 10) * pow(10,len-1)) > MAX || (reverse(x/10) + (x % 10) * pow(10,len-1)) < MIN)
+        return 0;
+    return reverse(x/10) + (x % 10) * pow(10,len-1);
 }
 
 int main(void)
 {
     int x = 321;
-    
+    cout << reverse(x) << endl;
     return 0;
-
 }
