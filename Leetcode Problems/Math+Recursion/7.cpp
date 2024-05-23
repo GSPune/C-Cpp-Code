7e424815f50cbe41a5e9b19abdf480814bc57773
Stephen George <stephengeorgepune@gmail.com>
2024-05-23 16:45:05 +0530

    Used tail recursion

--- Leetcode Problems/Math+Recursion/7.cpp
+++ Leetcode Problems/Math+Recursion/7.cpp
@@ -1,21 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int reverse(int x){
-    const int MAX = std::numeric_limits<int>::max();
-    const int MIN = std::numeric_limits<int>::min();
+int reverse(long long int x,long long int k){
+    const int MAX = numeric_limits<int>::max();
+    const int MIN = numeric_limits<int>::min();
     // int prev = x;
     if (x == 0)
-        return 0;
+        return k;
     int len = to_string(abs(x)).length();
-    if ((reverse(x/10) + (x % 10) * pow(10,len-1)) > MAX || (reverse(x/10) + (x % 10) * pow(10,len-1)) < MIN)
+    if (k > (MAX) || k < (MIN))
         return 0;
-    return reverse(x/10) + (x % 10) * pow(10,len-1);
+    return reverse(x / 10, k + ((x % 10) * pow(10, len - 1)));
+    //Tail Recursion
 }
 
 int main(void)
 {
-    int x = 321;
-    cout << reverse(x) << endl;
+    long long int x = -321;
+    cout << reverse(x,0) << endl;
     return 0;
 }
