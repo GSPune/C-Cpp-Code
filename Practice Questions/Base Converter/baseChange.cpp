593cd63ba78729d40cab6b43ba6f4e03b7b6c1f0
Stephen George <stephengeorgepune@gmail.com>
2024-05-28 23:23:43 +0530

    Added Base converter class

---
+++ Practice Questions/Base Converter/baseChange.cpp
@@ -0,0 +1,5 @@
+#include "baseChange.hpp"
+
+void baseChange::convert(){
+    
+}
---
+++ Practice Questions/Base Converter/baseChange.hpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class baseChange{
+    unsigned int n,k;
+    string kbase;
+    public:
+        baseChange();
+        void print();
+        void convertNumber(int);
+        void mapToChar(int);
+};
