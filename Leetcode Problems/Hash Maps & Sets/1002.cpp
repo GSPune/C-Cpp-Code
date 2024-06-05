070a180666dfe3fecc26db82f4de45807141a32d
Stephen George <stephengeorgepune@gmail.com>
2024-06-05 23:52:28 +0530

    Corrected Solution

--- Leetcode Problems/Hash Maps & Sets/1002.cpp
+++ Leetcode Problems/Hash Maps & Sets/1002.cpp
@@ -8,28 +8,35 @@ vector<string> commonChars(vector<string>& words) {
     if (words.size() == 1)
         return words;
     int n = words.size();
-   unordered_map<char,array<int,n>> freq;
-
-    // for(const auto &e:words){
-    //     for (const auto &t:e){
-    //         // cout << typeid(t).name() << endl;
-    //         // cout << t << " ";
-    //         freq[t]++;
-    //     }
-    // }
-
-    //  for(auto &u:freq){
-    //    if(u.second % 3 == 0){
-    //         for (int k = 0; k < (u.second / 3); ++k){
-    //             string random;
-    //             random.push_back(u.first);
-    //             cchars.push_back(random);
-    //         }
-    //    }
-    // }
-
-    return cchars;
+    //unordered_map<char,array<int,n>> freq;
+    vector<int> commonC(26),currentC(26);
     
+    for(const auto &e:words[0])
+        commonC[e-'a']++;
+    
+    for(int i = 0; i < n; ++i){
+        fill(currentC.begin(),currentC.end(),0);
+
+        // Count characters in the current word
+        for (char& ch : words[i]) {
+            currentC[ch - 'a']++;
+        }
+
+        for(int l = 0; l < 26;++l){
+            commonC[l] = min(commonC[l],currentC[l]);
+        }
+    }
+
+    // Collect the common characters based on the final counts
+    for (int l = 0; l < 26;++l) {
+        if(commonC[l] > 0){
+            for(int count = 0; count < commonC[l]; ++count){
+                cchars.push_back(string(1,l+'a'));
+            }
+        }
+    }
+
+    return cchars;  
 }
 
 int main(){
