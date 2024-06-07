e7371db5ea083d7ecdfadc6803b2f28d33bdcf44
Stephen George <stephengeorgepune@gmail.com>
2024-06-08 01:25:14 +0530

    Replace Words with Given Roots

---
+++ Leetcode Problems/Strings/648_Trie.cpp
@@ -0,0 +1,79 @@
+/*648.Replace Words*/
+
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+struct TrieNode{
+    TrieNode *child[26];
+    bool isEnd;
+    TrieNode(){
+        isEnd = false;
+        for(int i = 0; i < 26; i++)
+            child[i] = NULL;
+    }
+};
+
+void insert(string k,TrieNode *root){
+    TrieNode *curr = root;
+    for(int i = 0; i < k.length(); ++i){
+        int index = k[i] - 'a';
+        if(curr->child[index] == NULL)
+            curr->child[index] = new TrieNode();
+        curr = curr->child[index];
+    }
+    curr->isEnd = true;
+}
+
+string search(string k,TrieNode *root){
+    TrieNode *curr = root;
+    string match = "";
+    for(int i = 0; i < k.length(); ++i){
+        int index = k[i] - 'a';
+        if(curr->child[index] == NULL){
+            if(!curr->isEnd)
+                return "";
+            else
+                return match;
+        }
+        if(curr->isEnd)
+            return match;
+        match += k[i];
+        curr = curr->child[index];
+    }
+    if (curr->isEnd)
+        return match;
+    else
+        return "";
+}
+
+string replaceWords(vector<string>& dictionary, string sentence) {
+    int j;
+    TrieNode *r = new TrieNode();
+    for (const auto &w:dictionary){
+        insert(w,r);
+    }
+
+    string word,newSentence = "";
+    stringstream st(sentence);
+    while(st >> word){
+        string check = search(word,r);
+        if(check == "")
+            newSentence += word + " ";
+        else
+            newSentence += check + " ";
+    }
+
+    for (j = newSentence.size() - 1; j > -1; j--)
+        if (newSentence[j] != ' '){break;}
+
+    delete r;
+    return newSentence.substr(0,j+1);
+}
+
+int main(){
+    vector<string> dictionary = {"cat","catt","bat","rat"};
+    string sentence = "the cattle was rattled by the battery";
+    cout << replaceWords(dictionary,sentence);
+    return 0;
+}
