f796c61e58200dcefe1f0bc7f00da6ca4abfe9b3
Stephen George <stephengeorgepune@gmail.com>
2024-06-13 23:58:46 +0530

    Revised Version -- Time Limit Issue

--- Practice Questions/Codeforces/Div 4/952C.cpp
+++ Practice Questions/Codeforces/Div 4/952C.cpp
@@ -6,11 +6,15 @@ prefixes of the array a.*/
 using namespace std;
 
 void goodPrefixes(vector<int>& nums,int n){
-    int max = 0,sum = 0,count = 0;
-    for(int i = 0; i < n; ++i){
-        max = *max_element(nums.begin(),nums.begin()+i);
-        sum = accumulate(nums.begin(),nums.begin()+i,0);
-        if(max-sum == max)
+    int max = 0,count = 0;
+    long long sum = 0;
+    if (nums[0] == 0)
+        count++;
+    for(int i = 1; i < n; ++i){
+        max = *max_element(nums.begin(),nums.begin()+i+1);
+        //The result of accumulating init and all the elements in the range [first,last).
+        sum = accumulate(nums.begin(),nums.begin()+i+1,(long long)0);
+        if(sum - max == max)
             count++;
     }
     cout << count << endl;
