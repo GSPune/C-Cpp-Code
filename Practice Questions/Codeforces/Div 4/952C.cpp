6f4a045457f799b83a0d6f72910107b31ec202f7
Stephen George <stephengeorgepune@gmail.com>
2024-06-14 00:10:20 +0530

    Updated Solution -- O(n) Solution

--- Practice Questions/Codeforces/Div 4/952C.cpp
+++ Practice Questions/Codeforces/Div 4/952C.cpp
@@ -6,15 +6,14 @@ prefixes of the array a.*/
 using namespace std;
 
 void goodPrefixes(vector<int>& nums,int n){
-    int max = 0,count = 0;
+    int mx = 0,count = 0;
     long long sum = 0;
-    if (nums[0] == 0)
-        count++;
-    for(int i = 1; i < n; ++i){
-        max = *max_element(nums.begin(),nums.begin()+i+1);
-        //The result of accumulating init and all the elements in the range [first,last).
-        sum = accumulate(nums.begin(),nums.begin()+i+1,(long long)0);
-        if(sum - max == max)
+    // if (nums[0] == 0)
+    //     count++;
+    for(int i = 0; i < n; ++i){
+        mx = max(mx,nums[i]);
+        sum += nums[i];
+        if(sum - mx == mx)
             count++;
     }
     cout << count << endl;
