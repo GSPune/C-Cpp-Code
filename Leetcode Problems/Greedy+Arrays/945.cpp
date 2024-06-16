43ed0f1263c5ec16bb263b985f37de48e6e20a28
Stephen George <stephengeorgepune@gmail.com>
2024-06-16 09:09:04 +0530

    Minor change

--- Leetcode Problems/Greedy+Arrays/945.cpp
+++ Leetcode Problems/Greedy+Arrays/945.cpp
@@ -8,11 +8,7 @@ public:
             sort(nums.begin(),nums.end());
             int sz = nums.size(), k = 0;
             for(int i = 0; i < sz - 1; ++i){
-                if (nums[i] == nums[i+1]){
-                    k++;
-                    nums[i+1] = nums[i] + 1;
-                }
-                if(nums[i+1] < nums[i]){
+                if(nums[i+1] <= nums[i]){
                     k += (nums[i] + 1 - nums[i+1]);
                     nums[i+1] = nums[i] + 1;
                 }
