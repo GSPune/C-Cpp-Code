c59b52eb1bcd36c3ff9ff13ea12a1cb5f5cf0a36
Stephen George <stephengeorgepune@gmail.com>
2024-06-25 21:14:22 +0530

    Possible Tie Problem?

---
+++ Practice Questions/Codeforces/Div 2/955A.cpp
@@ -0,0 +1,29 @@
+//A. Soccer (Div2).
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    long x1,x2,y1,y2;
+    cin >> x1 >> y1;
+    cin >> x2 >> y2;
+
+    // if(y1 < x1 && y1 < x2 && y2 < x1 && y2 < x2){
+    //     cout << "YES" << endl;
+    // }
+    if(x1 < y1 && y1 < x2 && x1 < y2 && y2 < x2){
+        cout << "NO" << endl;
+    }
+    else
+        cout << "YES" << endl;
+}
+
+int main(){
+    int cases;
+    //Number of test cases
+    cin >> cases;
+    for (int i = 0; i < cases; i++){
+        solve();
+    }
+    return 0;
+}
