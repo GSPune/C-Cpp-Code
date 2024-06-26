bd664065430addcb63b357cba7af0c2a6affefc3
Stephen George <stephengeorgepune@gmail.com>
2024-06-26 07:00:57 +0530

    Corrections added

--- Practice Questions/Codeforces/Div 2/955A.cpp
+++ Practice Questions/Codeforces/Div 2/955A.cpp
@@ -11,7 +11,10 @@ void solve(){
     // if(y1 < x1 && y1 < x2 && y2 < x1 && y2 < x2){
     //     cout << "YES" << endl;
     // }
-    if(x1 < y1 && y1 < x2 && x1 < y2 && y2 < x2){
+    if(x1 < y1 && y1 < x2 && x1 < y2 && y2 < x2){ //simpler is just x1 < y1 && y2 < x2
+        cout << "NO" << endl;
+    }
+    else if(y1 < x1 && x2 < y2){
         cout << "NO" << endl;
     }
     else
