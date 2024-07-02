6ca9c02a68e848d1b8e603034c12f090a8f84d66
Stephen George <stephengeorgepune@gmail.com>
2024-07-02 23:14:49 +0530

    Corrected Solution

--- Leetcode Problems/Math+Recursion/519.cpp
+++ Leetcode Problems/Math+Recursion/519.cpp
@@ -14,25 +14,33 @@ public:
 
     vector<int> flip() {
         vector<int> indices;
-        if (lc == (cols - 1)) {
-            lr++;
+        lc++;
+        if(lc == cols){
             lc = 0;
-        }
-        //kinda reset
-        else if(lr == (rows -1)){
+            lr++;
+        } 
+        if(lr == rows){
             lc = lr = 0;
-        }
-        else {
-            lc++;// will iterate over columns till we can
-        }
+        }      
         indices.push_back(lr);
         indices.push_back(lc);
         return indices;
     }
 
-    void reset() {}
+    void reset() {
+        //When reset is called we will not alter the iterators so probability will remain eqally likeable.
+    }
 };
 
 int main(){
+    Solution *obj = new Solution(3,1);
+    vector<int> param_1 = obj->flip();
+    for(auto &e:param_1) cout << e << " ";
+    vector<int> param_2 = obj->flip();
+    for(auto &e:param_2) cout << e << " ";
+    vector<int> param_3 = obj->flip();
+    for(auto &e:param_3) cout << e << " ";
+    vector<int> param_4 = obj->flip();
+    for(auto &e:param_4) cout << e << " ";
     return 0;
 }
