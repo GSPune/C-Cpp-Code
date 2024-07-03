89968c74b7f52286b73f87fc22165828d3b7bf9b
Stephen George <stephengeorgepune@gmail.com>
2024-07-04 00:53:02 +0530

    2nd Try

--- Leetcode Problems/DP+Backtracking/37.cpp
+++ Leetcode Problems/DP+Backtracking/37.cpp
@@ -2,23 +2,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool stopRev = false;
 class Solution {
 private:
-    static const int N = 9;
+    static const int N = 4;// = 9;
 public:
     bool isSafe(vector<vector<char>>& board,int r,int c,int nums){
         //checking the row and column c
         for(int i = 0; i < N; ++i){
-            if(board[r][i] == nums || board[i][c] == nums)
+            if(board[r][i] == char(nums+'0') || board[i][c] == char(nums+'0'))
                 return false;
         }
 
         //checking the sub-grids
-        int rS = r - (r%3);
-        int cS = c - (c%3);
-        for(int i = rS; i < (rS+3); ++i){
-            for(int j = cS; j < (cS+3); ++j){
-                if(board[i][j] == nums)
+        int s = sqrt(N);
+        int rS = r - (r%s);
+        int cS = c - (c%s);
+        for(int i = rS; i < (rS+s); ++i){
+            for(int j = cS; j < (cS+s); ++j){
+                if(board[i][j] == char(nums+'0'))
                     return false;
             }
         }
@@ -28,29 +30,49 @@ public:
 
     void solveSudoku(vector<vector<char>>& board) {
         int r = -1,c = -1;
-        bool isEmpty = false; // finding an empty cell
+        bool isEmpty = false; //, stopRev = false; // finding an empty cell
 
         for(int i = 0; i < N; ++i){
             for(int j = 0; j < N; ++j){
-                if(board[i][j] = '.'){
+                if(board[i][j] == '.'){
                     r = i; c = j;
                     isEmpty = true;
                     break;
                 }
             }
+            if(isEmpty)
+                break;
         }
 
         //No empty cell found..STOP!
-        if(!isEmpty)
+        if(!isEmpty){
+            stopRev = true;
             return;
+        }
 
-        for(int i = 1; i < 10; ++i){
+        for(int i = 1; i <= N; ++i){
             if(isSafe(board,r,c,i)){
                 board[r][c] = char(i+'0');
                 solveSudoku(board);
-                board[r][c] = '.';
+                if(!stopRev)
+                    board[r][c] = '.';
             }  
         }
         return;
     }
 };
+
+int main(){
+    // Solution *obj = new Solution();
+    Solution obj;
+    vector<vector<char>> T = {{'1','.','3','.'},{'.','.','2','1'},{'.','1','.','2'},{'2','4','.','.'}};
+    obj.solveSudoku(T);
+    for(const auto &e:T){
+        for(const auto &u:e){
+            cout << u << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    return 0;
+}
