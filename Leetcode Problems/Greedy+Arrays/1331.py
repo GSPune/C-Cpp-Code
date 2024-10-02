aaa9868f35951faf89737da168ff7c95265a2d6a
Stephen George <stephengeorgepune@gmail.com>
2024-10-03 00:47:38 +0530

    Rank Transform

--- Leetcode Problems/Greedy+Arrays/1331.py
+++ Leetcode Problems/Greedy+Arrays/1331.py
@@ -1,17 +1,16 @@
 # 1331. Rank Transform of an Array
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
-        # print(max(arr))
-        # ordered_values = list(set(sorted(arr)))
-        ordered_values, ranks = [], []
-        a = sorted(arr)
-        for t in a:
-            if t not in ordered_values:
-                ordered_values.append(t)
-        
-        print(ordered_values)
+        mapRanks = {}
+        sorted_array = sorted(set(arr))
+        rank = 1
+        l = len(sorted_array)
+        for i in range(l):
+            mapRanks[sorted_array[i]] = i+1        
+        # print(mapRanks)
 
-        for e in arr:
-            i = ordered_values.index(e) + 1
-            ranks.append(i)
-        return ranks
+        for i in range(len(arr)):
+            arr[i] = mapRanks[arr[i]]
+        return arr
+
+        # Time Complexity: O(N⋅logN)
